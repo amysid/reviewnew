687062620e69eb9e9ef5fecfdf953519e5342be1
shubham <ro-shubham@mobiloitte.com>
2019-06-20 12:37:52 +0530

    html integration in login page

--- app/assets/javascripts/admin/users.js
+++ app/assets/javascripts/admin/users.js
@@ -1,17 +1,17 @@
-// csv file validations
-$(document).ready(function(){
-const readSingleFile = () => {
- const fileInput = document.getElementById("fileinput");
- const form = document.getElementById("controls-form");
+// // csv file validations
+// $(document).ready(function(){
+// const readSingleFile = () => {
+//  const fileInput = document.getElementById("fileinput");
+//  const form = document.getElementById("controls-form");
 
- if (fileInput.files[0].type !== "text/csv") {
-   alert("Accept only CSV file");
-   form.reset();
- } 
-};
-// Some sweet event listeners!
-document
- .getElementById("fileinput")
- .addEventListener("change", readSingleFile, false);
-});
-// csv file validations end..
+//  if (fileInput.files[0].type !== "text/csv") {
+//    alert("Accept only CSV file");
+//    form.reset();
+//  } 
+// };
+// // Some sweet event listeners!
+// document
+//  .getElementById("fileinput")
+//  .addEventListener("change", readSingleFile, false);
+// });
+// // csv file validations end..
