4da8a808006109b5e3617e31693a9e84b04697f1
Lalit Saini <ro-lalit@mobiloitte.com>
2019-06-27 14:00:17 +0530

    add mailer in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,6 @@ Rails.application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
-  config.action_mailer.default_url_options = { :host => "https://reviewsystem.herokuapp.com" }
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
@@ -94,4 +93,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { :host => "https://reviewsystem.herokuapp.com" }
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:             Rails.application.credentials.MAILER_USER_NAME,
+    password:              Rails.application.credentials.MAILER_PASSWORD,
+    authentication:       :plain,
+    enable_starttls_auto: true
+  }
+
 end
