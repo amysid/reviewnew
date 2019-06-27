ad4793a186af24808995fbea2f542607548bd172
shubham <ro-shubham@mobiloitte.com>
2019-06-27 13:33:20 +0530

    update production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,7 +99,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'https://reviewsystem.herokuapp.com/', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://reviewsystem.herokuapp.com/' }
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
