04f053f3259e8339d447a9bb8f79a151008f016c
esha pradhan <ro-eshapradhan@mobiloitte.com>
2019-06-27 19:19:20 +0530

    alert for lofing signup and forget password login confermation

--- app/controllers/web/users_controller.rb
+++ app/controllers/web/users_controller.rb
@@ -1,5 +1,10 @@
 class Web::UsersController < ApplicationController
   def index
+  	#binding.pry	
+  	if (current_user.present? && current_user.role == "admin")
+        redirect_to admin_home_index_path(current_user)
+  	end
+  	
   end
 
   def show
