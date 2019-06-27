75b154737cf58762abe7ab681870f35f57ae872f
shubham <ro-shubham@mobiloitte.com>
2019-06-27 13:21:35 +0530

    update code on master branch

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key  = '6LdeoKkUAAAAAApIuVDcSocius0cOW7-t2kLXJ6q'
-  config.secret_key = '6LdeoKkUAAAAAHIF8urywkibIRt5T-paZWapeITx'
+  config.site_key  = '6Lcv7KoUAAAAADpQhuejDVr0UPN0c6IYZj0vYhes'
+  config.secret_key = '6Lcv7KoUAAAAAGFNP9XmkPZLK1_SbtF58M2LVOxO'
 end
