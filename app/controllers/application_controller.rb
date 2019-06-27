cf45b874f0539959270cc95c54a7edfd4248f562
shubham <ro-shubham@mobiloitte.com>
2019-06-27 09:08:47 +0530

    update master branch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < ActionController::Base
     def after_sign_in_path_for(resource)
       # binding.pry
        if(resource.role == "admin")
-        return  admin_home_index_path(current_user)
+        return  admin_homes_index_path
        else  
         super
        end
