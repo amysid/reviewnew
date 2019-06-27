f1505300e115737a81cdca1e184ff7c1864e3a72
shubham <ro-shubham@mobiloitte.com>
2019-06-27 16:55:22 +0530

    update production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:             Rails.application.credentials.MAILER_USER_NAME,
+    password:              Rails.application.credentials.MAILER_PASSWORD,
+    authentication:       :plain,
+    enable_starttls_auto: true
+  }
 end
