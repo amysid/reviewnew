fdef91d392a4571d4614755c02d88d1c963d74c9
Lalit Saini <ro-lalit@mobiloitte.com>
2019-07-19 20:25:20 +0530

    update table for review parts and get review on same page

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ApplicationRecord
 	has_many :sub_categories,autosave: true, dependent: :destroy
 	has_many :product, dependent: :destroy
+	has_many :review_parts, dependent: :destroy
 	# accepts_nested_attributes_for :sub_categories, allow_destroy: true
 validates :category_name, length: { minimum: 2, maximum: 25}
 end
--- app/models/review_part.rb
+++ app/models/review_part.rb
@@ -1,3 +1,3 @@
 class ReviewPart < ApplicationRecord
-  belongs_to :product
+  belongs_to :category
 end
