bc07c73c1afc57b338ddd5834e76e3fbb7a63b0f
atul <ro-atultiwari@mobiloitte.com>
2019-08-29 11:47:51 +0530

    update code

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,5 +3,5 @@ class Category < ApplicationRecord
 	has_many :product, dependent: :destroy
 	has_many :review_parts, dependent: :destroy
 	# accepts_nested_attributes_for :sub_categories, allow_destroy: true
-validates :category_name, length: { minimum: 2, maximum: 25}
+validates :category_name, length: { minimum: 2, maximum: 30}
 end
--- app/models/sub_category.rb
+++ app/models/sub_category.rb
@@ -1,7 +1,7 @@
 class SubCategory < ApplicationRecord
 	belongs_to :category
 	has_many :products, dependent: :destroy
-	validates :sub_category_name, length: { minimum: 5, maximum: 25}
+	validates :sub_category_name, length: { minimum: 5, maximum: 30}
 	# has_many :details,autosave: true,dependent: :destroy
     # accepts_nested_attributes_for :details, allow_destroy: true
     
