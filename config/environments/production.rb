0138569c2c287e88475a26e9ecc71964e2c506a7
avdhesh.kumar <ro-avdhesh@mobiloitte.com>
2019-10-01 04:58:31 +0000

    code push server to local

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'http://review.cmex.network' }
+  config.action_mailer.default_url_options = { host: 'https://lina.review' }
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
